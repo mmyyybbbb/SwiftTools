9453f3303b9936dd9bbc414e7a40e10dd34b0fbf
Alexej Nenastev <alexey.ne.bcs@mail.ru>
2019-07-19 13:45:05 +0300

    uidevice+

---
+++ SwiftTools/Device+/Device+.swift
@@ -0,0 +1,22 @@
+//
+//  Device+.swift
+//  SwiftTools
+//
+//  Created by alexej_ne on 19.07.2019.
+//  Copyright © 2019 alexeyne. All rights reserved.
+//
+
+public extension UIDevice {
+    
+    static func installationId() -> String {
+        let key = "__installationId"
+        
+        if let id = KeyChainService.load(key: key) {
+            return id
+        }
+        
+        let id: String = ProcessInfo().globallyUniqueString
+        KeyChainService.set(id, forKey: key)
+        return id
+    }
+}
---
+++ SwiftTools/Keychain/KeyChainService.swift
@@ -0,0 +1,61 @@
+//
+//  KeyChainService.swift
+//  BrokerDomainCore
+//
+//  Created by Andrey Raevnev on 28/02/2019.
+//  Copyright © 2019 BCS. All rights reserved.
+//
+
+public final class KeyChainService {
+    
+    @discardableResult
+    public class func set(_ id: String, forKey: String) -> Bool {
+        if let data = id.data(using: String.Encoding.utf8) {
+            let query: [String : Any] = [
+                kSecClass as String       : kSecClassGenericPassword as String,
+                kSecAttrAccount as String : forKey,
+                kSecValueData as String   : data ]
+            
+            SecItemDelete(query as CFDictionary)
+            let status = SecItemAdd(query as CFDictionary, nil)
+            if status == noErr {
+                return true
+            }
+        }
+        return false
+    }
+    
+    public class func load(key: String) -> String? {
+        let query: [String : Any] = [
+            kSecClass as String       : kSecClassGenericPassword,
+            kSecAttrAccount as String : key,
+            kSecReturnData as String  : kCFBooleanTrue,
+            kSecMatchLimit as String  : kSecMatchLimitOne ]
+        
+        var dataTypeRef: AnyObject? = nil
+        
+        let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        
+        if status == noErr, let receivedData = dataTypeRef as? Data, let id = String(data: receivedData, encoding: String.Encoding.utf8) {
+            return id
+        }
+        return nil
+    }
+    
+    #if DEBUG
+    @discardableResult
+    public class func remove(forKey: String) -> Bool {
+        let query: [String : Any] = [
+            kSecClass as String       : kSecClassGenericPassword as String,
+            kSecAttrAccount as String : forKey
+        ]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        if status == noErr {
+            return true
+        }
+        return false
+    }
+    #endif
+    
+}
