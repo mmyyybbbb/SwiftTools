81faa8f6523c41c4f8f3d288fe28f6a43bdf171d
Alexej Nenastev <alexey.ne.bcs@mail.ru>
2019-07-31 17:20:35 +0300

    helpers

---
+++ SwiftTools/Swift+/Sequence+.swift
@@ -0,0 +1,31 @@
+//
+//  Sequence+.swift
+//  SwiftTools
+//
+//  Created by alexej_ne on 31.07.2019.
+//  Copyright © 2019 alexeyne. All rights reserved.
+//
+
+public extension Sequence {
+    func groupSort(ascending: Bool = true, byDate dateKey: (Iterator.Element) -> Date) -> [[Iterator.Element]] {
+        var categories: [[Iterator.Element]] = []
+        for element in self {
+            let key = dateKey(element)
+            guard let dayIndex = categories.firstIndex(where: { $0.contains(where: { Calendar.current.isDate(dateKey($0), inSameDayAs: key) }) }) else {
+                guard let nextIndex = categories.firstIndex(where: { $0.contains(where: { dateKey($0).compare(key) == (ascending ? .orderedDescending : .orderedAscending) }) }) else {
+                    categories.append([element])
+                    continue
+                }
+                categories.insert([element], at: nextIndex)
+                continue
+            }
+            
+            guard let nextIndex = categories[dayIndex].firstIndex(where: { dateKey($0).compare(key) == (ascending ? .orderedDescending : .orderedAscending) }) else {
+                categories[dayIndex].append(element)
+                continue
+            }
+            categories[dayIndex].insert(element, at: nextIndex)
+        }
+        return categories
+    }
+}
