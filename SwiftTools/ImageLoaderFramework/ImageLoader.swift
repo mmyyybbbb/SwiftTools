17d1dbc9ecc63c3f9590f8edb4f7b41a981a0a4a
Andrey Raevnev <raevnev.bcs@yandex.ru>
2019-12-09 16:42:49 +0300

    MB-3952

--- SwiftTools/ImageLoaderFramework/ImageLoader.swift
+++ SwiftTools/ImageLoaderFramework/ImageLoader.swift
@@ -20,9 +20,7 @@ public class ImageLoader {
     }
     
     public func load(by url: URL, completion: @escaping ((UIImage?) -> Void)) {
-        let capturedUrl = url
         if let image = imageCache.object(forKey: url.absoluteString as NSString) {
-            guard capturedUrl == url else { return }
             completion(image)
             return
         }
@@ -30,22 +28,16 @@ public class ImageLoader {
             do {
                 let data = try Data(contentsOf: url)
                 let image = UIImage(data: data) ?? UIImage()
-                DispatchQueue.main.async { [weak self] in
-                    self?.imageCache.setObject(image, forKey: url.absoluteString as NSString)
-                    
-                    guard capturedUrl == url else { return }
+                DispatchQueue.main.async {
+                    self.imageCache.setObject(image, forKey: url.absoluteString as NSString)
                     completion(image)
                 }
             } catch {
-                completion(nil)
                 debugPrint("ImageLoader error! \(url.absoluteString) - is not image url")
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
             }
         }
-        
-        UIImage.loadFromDirectory(dirName: "T##String") { dict in
-            
-            
-        }
-        
     }
 }
