7abdfb860f3895e9aac66a4a5251531a5431efe4
George Prokopchuk <thewesterndoom@gmail.com>
2020-07-08 16:59:01 +0300

    PREM-49

--- BCSSwiftTools/Swift+/Array+.swift
+++ BCSSwiftTools/Swift+/Array+.swift
@@ -36,3 +36,12 @@ public extension Array where Element: Equatable {
         }
     }
 }
+
+public extension Array {
+    
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
