a47be815882684930eb607c00760bb075bb987bc
Omikron91 <Omikron91@mail.ru>
2020-09-29 16:49:09 +0700

    MB-10255

---
+++ BCSSwiftTools/Swift+/NSAttributedString+.swift
@@ -0,0 +1,23 @@
+//
+//  NSAttributedString+.swift
+//  BCSSwiftTools
+//
+//  Created by aryzhov on 29.09.2020.
+//  Copyright © 2020 alexeyne. All rights reserved.
+//
+
+public extension NSAttributedString {
+    func height(withConstrainedWidth width: CGFloat) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, context: nil)
+
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, context: nil)
+
+        return ceil(boundingBox.width)
+    }
+}
