c9dcedb001e288ed1ce25b11af84f75017a3f732
Andrey Raevnev <andrey.raevnev@gmail.com>
2020-12-08 00:44:05 +0300

    PersistOptional

--- BCSSwiftTools/Persist/Persist.swift
+++ BCSSwiftTools/Persist/Persist.swift
@@ -24,3 +24,25 @@ public struct Persist<T> {
         self.defaultValue = defaultValue
     }
 }
+
+@propertyWrapper
+public struct PersistOptional<T> {
+    public let key: String
+    public let defaultValue: T?
+    
+    public var wrappedValue: T? {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            if let newValue = newValue {
+                UserDefaults.standard.set(newValue, forKey: key)
+            }
+        }
+    }
+    
+    public init(key: String, defaultValue: T?) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+}
