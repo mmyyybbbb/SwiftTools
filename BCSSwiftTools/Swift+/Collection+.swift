68a4b44791b75760ebd726051c5eb696c0d5c461
Svyatoshenko "Megal" Misha <1654478+Megal@users.noreply.github.com>
2021-02-18 08:59:25 +0300

    Safe subscript made public

--- BCSSwiftTools/Swift+/Collection+.swift
+++ BCSSwiftTools/Swift+/Collection+.swift
@@ -8,7 +8,7 @@
 extension Collection {
 
     /// Returns the element at the specified index if it is within bounds, otherwise nil.
-    subscript (safe index: Index) -> Element? {
+    public subscript (safe index: Index) -> Element? {
         indices.contains(index) ? self[index] : nil
     }
 }
